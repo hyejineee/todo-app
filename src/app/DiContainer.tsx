c063c7eebd25aef43958d193a50ba2899b6ab333
현혜진 <hyeonhyejin@hyeonhyejin-ui-Macmini.local>
2024-11-23 22:45:15 +0900

    refac : di container edit

--- src/app/DiContainer.tsx
+++ src/app/DiContainer.tsx
@@ -12,13 +12,13 @@ import { createContext, useContext, type ReactNode } from 'react';
 
 // TODO : DiContainer를 context로 만들어야 하는가? 그냥 상위 파일에 선언해서 사용할 수 있는거 아닌가...? 리액트와 상관없는데...??
 
-const httpClient = new HttpClient('http://localhost:4000');
+export const httpClient = new HttpClient('http://localhost:4000');
 
-const authDataSource = new AuthRemoteDataSource(httpClient);
-const tokenDataSource = new TokenLocalDataSource();
+export const authDataSource = new AuthRemoteDataSource(httpClient);
+export const tokenDataSource = new TokenLocalDataSource();
 
-const authRepository = new AuthRepository(authDataSource);
-const tokenRepository = new TokenRepository(tokenDataSource);
+export const authRepository = new AuthRepository(authDataSource);
+export const tokenRepository = new TokenRepository(tokenDataSource);
 
 interface DiContainer {
   httpClient: HttpClient;
