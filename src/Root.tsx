2ce3c1abe5ce64e4f14bff6f21b1029021b7dbbd
현혜진 <hyeonhyejin@hyeonhyejin-ui-Macmini.local>
2024-12-01 18:44:43 +0900

    chore : emotion 라이브러리 사용을 위한 설정

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,25 @@
 import 'reflect-metadata';
 
+import { css, Global } from '@emotion/react';
 import { router } from '@shared/config';
 import { RouterProvider } from 'react-router-dom';
+import './index.css';
 
 function Root() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Global
+        styles={css`
+          *,
+          *::after,
+          *::before {
+            box-sizing: border-box;
+          }
+        `}
+      />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default Root;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: [
+          // 데코레이터 과련
+          ['@babel/plugin-syntax-decorators', { legacy: true }],
+          '@babel/plugin-syntax-class-properties',
+
+          // emotion
+          '@emotion/babel-plugin',
+        ],
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
