da82a4c0c76ccce00e4aa39b57e39a13e11501bf
현혜진 <hyeonhyejin@hyeonhyejin-ui-Macmini.local>
2024-12-01 21:16:18 +0900

    feat : todo 페이지 레이아웃 작업

--- src/presentation/pages/TodosPage.tsx
+++ src/presentation/pages/TodosPage.tsx
@@ -1,10 +1,16 @@
-import { Row } from '@shared/ui';
+import { Row } from '@/shared/ui';
+import { Card } from '@/shared/ui/components/card';
 import { Outlet } from 'react-router-dom';
 
 export const TodosPage = () => {
   return (
-    <Row>
-      todo list page <Outlet />
+    <Row css={{ padding: 24, width: '100%', height: '100%', gap: 16 }}>
+      <Card css={{ width: '35%', height: '100%', padding: 16 }}>
+        todo list page
+      </Card>
+      <Card css={{ width: '65%', height: '100%', padding: 16 }}>
+        <Outlet />
+      </Card>
     </Row>
   );
 };
